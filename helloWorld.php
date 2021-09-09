f061b2ed7c551e1ee4f559c4c53f9f932058cfa9
jhgullion <64504726+jhgullion@users.noreply.github.com>
2021-09-09 13:38:26 -0500

    First copy of this file in remote repo

---
+++ helloWorld.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>WDV341 Intro PHP</h1>
+    <h2>PHP Basics</h2>
+
+    <?php
+
+        //These comments will not be sent to the server
+        //because they are not 'echo' or 'write' to the reponse object
+
+        //echo "<h3>This output comes from PHP</h3>";
+
+    ?>
+
+</body>
+</html>
+
